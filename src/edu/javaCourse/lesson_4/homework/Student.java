432a7c4623877ebb3271852c6f2dc33a0f03eb79
Saheme <roj_t@mail.ru>
2023-05-04 17:24:53 +0300

    lesson_4_homework

---
+++ src/edu/javaCourse/lesson_4/homework/Student.java
@@ -0,0 +1,11 @@
+package edu.javaCourse.lesson_4.homework;
+
+public class Student {
+    int studentId;
+    String firstName;
+    String lastName;
+    int academicLevel; //год обучения
+    double averageGradeInMath;
+    double averageGradeInEconomy;
+    double averageGradeInForeignLanguage;
+}
