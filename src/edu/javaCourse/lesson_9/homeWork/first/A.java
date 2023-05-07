c09023460f267c00a929b7711f40ca456b543244
Saheme <roj_t@mail.ru>
2023-05-07 12:35:17 +0300

    lesson__9_homework

---
+++ src/edu/javaCourse/lesson_9/homeWork/first/A.java
@@ -0,0 +1,23 @@
+package edu.javaCourse.lesson_9.homeWork.first;
+
+public class A {
+
+}
+class B{
+    public static void main(String[] args) {
+        A ob1 = new A();
+        A ob2 = new A();
+        A ob3 = new A();
+        A ob4 = new A();
+        A ob5 = new A();
+        A ob6 = new A();
+        A ob7 = new A();
+        A ob8 = new A();
+        ob1 = null;
+        ob2 = null;
+        ob3 = null;
+        ob4 = null;
+        ob5 = null;
+        ob6 = null;
+    }
+}
---
+++ src/edu/javaCourse/lesson_9/homeWork/second/Test1.java
@@ -0,0 +1,19 @@
+package edu.javaCourse.lesson_9.homeWork.second;
+
+/**
+ * напечатает 3 и 1
+ */
+public class Test1 {
+    //int a = 1;
+    static int a = 2;
+
+    void abc(int a) {
+        System.out.println(a);
+        System.out.println(this.a);
+    }
+
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+        t.abc(3);
+    }
+}
