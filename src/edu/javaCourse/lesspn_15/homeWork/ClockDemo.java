47d21db2911fd655f1b439dc6e6685ca8dc9d113
Saheme <roj_t@mail.ru>
2023-05-10 10:11:19 +0300

    lesson_15_homeWork

---
+++ src/edu/javaCourse/lesspn_15/homeWork/ClockDemo.java
@@ -0,0 +1,30 @@
+package edu.javaCourse.lesspn_15.homeWork;
+
+public class ClockDemo {
+   public static void clock(){
+        int hour = 0;
+        OUTER:
+        while (hour < 6) {
+            int minutes = -1;
+            MIDDLE:
+            do {
+                int secunds = 0;
+                minutes++;
+                if (hour > 1 && minutes % 10 == 0) {
+                    break OUTER;
+                }
+                while (secunds < 60) {
+                    if (secunds * hour > minutes) {
+                        continue MIDDLE;
+                    }
+                    System.out.println(hour + " : " + minutes + " : " + secunds);
+                    secunds++;
+                }
+
+
+            } while (minutes < 59);
+            hour++;
+        }
+    }
+}
+
---
+++ src/edu/javaCourse/lesspn_15/homeWork/ClockDemoTest.java
@@ -0,0 +1,7 @@
+package edu.javaCourse.lesspn_15.homeWork;
+
+public class ClockDemoTest {
+    public static void main(String[] args) {
+        ClockDemo.clock();
+    }
+}
