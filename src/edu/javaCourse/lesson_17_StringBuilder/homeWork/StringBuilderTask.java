fa43059fcd262ff6c9153e17d4394053f7be226a
Saheme <roj_t@mail.ru>
2023-05-11 20:02:40 +0300

    lesson_17_homeWork

---
+++ src/edu/javaCourse/lesson_17_StringBuilder/homeWork/StringBuilderTask.java
@@ -0,0 +1,20 @@
+package edu.javaCourse.lesson_17_StringBuilder.homeWork;
+
+public class StringBuilderTask {
+
+    public static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2){
+        String str1 = new String(sb1);
+        String str2 = new String(sb2);
+        if (str1.equals(str2))
+        return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("Hello");
+        StringBuilder sb2 = new StringBuilder("Hello");
+
+        boolean b1 = ravenstvo(sb1,sb2);
+        System.out.println(b1);
+    }
+}
