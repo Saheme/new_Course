d1e3c8d9bfa9a1771a189f6d17627404dfd8d022
Saheme <roj_t@mail.ru>
2023-05-12 10:45:00 +0300

    lesson_17_homeWork(dop)

--- src/edu/javaCourse/lesson_17_StringBuilder/homeWork/StringBuilderTask.java
+++ src/edu/javaCourse/lesson_17_StringBuilder/homeWork/StringBuilderTask.java
@@ -12,11 +12,29 @@ public class StringBuilderTask {
         }
     }
 
+    public static boolean ravenstvo2(StringBuilder sb1, StringBuilder sb2) {
+        boolean result = true;
+        if (sb1.length() == sb2.length()) {
+            for (int i = 0; i < sb1.length(); i++) {
+                 if (sb1.charAt(i) != sb2.charAt(i)) {
+                    result = false;
+                    break;
+                }
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
         StringBuilder sb1 = new StringBuilder("Hello");
         StringBuilder sb2 = new StringBuilder("Hello");
 
         boolean b1 = ravenstvo(sb1, sb2);
         System.out.println(b1);
+
+        System.out.println(ravenstvo2(sb1,sb2));
     }
 }
